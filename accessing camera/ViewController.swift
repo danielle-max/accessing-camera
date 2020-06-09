be6a08faf5b9d6f7f6860766ab5301bf00ccfcfa
Danielle <danielleho27874@gmail.com>
2020-06-09 10:41:48 -0400

    Initial Commit

---
+++ accessing camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  accessing camera
+//
+//  Created by Danielle Ho on 6/9/20.
+//  Copyright © 2020 Danielle Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
